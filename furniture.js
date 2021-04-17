63e33ec39cb67606412b22c7f923abded1b4c6f5
Stefan Schwaha <the.stevee@gmail.com>
2021-04-17 18:39:09 +0200

    two objects

--- furniture.js
+++ furniture.js
@@ -1,23 +1,38 @@
 class Furniture {
-  constructor(x, y) {
+  constructor(x, y, image, resize_factor, target_link) {
     this.x = x;
     this.y = y;
+    this.image = image;
+    this.resize_factor = resize_factor;
+    this.target_link = target_link;
 
-    this.width = 30;
-    this.height = 60;
+    this.image.width_origin = this.image.width;
+    this.image.height_origin = this.image.height;
+    this.image.resize(this.image.width_origin / this.resize_factor, this.image.height_origin / this.resize_factor);
+
   }
 
   draw() {
-    push();
-    fill("#304858");
-    rect(this.x, this.y, this.width, this.height);
-    pop();
+    // needs to be called in the loop
+    this.if_hover = (mouseX >= this.x && mouseX <= (this.x + this.image.width) && mouseY >= this.y && mouseY <= (this.y + this.image.height))
+
+    image(this.image, this.x, this.y);
+
+    this.hover();
+  }
+
+  // overall function - loop through all objects
+  hover() {
+    if (this.if_hover) {
+      cursor(HAND);
+    } else {
+      cursor(ARROW);
+    }
   }
 
   clicked() {
-    if (mouseX >= this.x && mouseX <= (this.x + this.width) && mouseY >= this.y && mouseY <= (this.y + this.height)) {
-      console.log("clicked");
-      window.open('http://orf.at')
+    if (this.if_hover) {
+      window.open(this.target_link)
     }
   }
 }
--- sketch.js
+++ sketch.js
@@ -3,27 +3,33 @@ let canvasWidth = 1000;
 let canvasHeight = 800;
 
 let house;
-let tv;
-
+let telephone;
+let whiteboard;
+let telephone_image;
+let whiteboard_image;
 
 function preload() {
   // soundtrack = loadSound('Sunny Day-SoundBible.com-2064222612.mp3');
   house = loadImage('images/house_dummy.jpg');
+  telephone_image = loadImage('images/telephone.png');
+  whiteboard_image = loadImage('images/whiteboard.png');
 }
 
 function setup() {
   createCanvas(canvasWidth, canvasHeight);
 
-  tv = new Furniture(50, 50);
+  telephone = new Furniture(50, 50, telephone_image, 30, 'https://teams.microsoft.com/l/meetup-join/19%3ameeting_YzA4YzdhMDAtMWUwYi00MmExLWEzZWMtMzU4Mjk5ODQ3YTJj%40thread.v2/0?context=%7b%22Tid%22%3a%22452b551c-4057-4e63-a6fe-63641fc132f9%22%2c%22Oid%22%3a%22e79d3def-9ee9-413a-b242-93cfd991115d%22%7d');
+  whiteboard = new Furniture(250, 250, whiteboard_image, 4, "https://orf.at");
 }
 
 function draw() {
 
   background("red");
-
-  tv.draw();
-
   image(house, (width/2 - house.width/2), (height - house.height));
+
+  telephone.draw();
+  whiteboard.draw();
+
 
   // text
   // fill("#7b3705");
@@ -36,5 +42,6 @@ function draw() {
 
 function mousePressed() {
   // soundtrack.play();
-  tv.clicked();
+  telephone.clicked();
+  whiteboard.clicked();
 }
